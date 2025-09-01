f892ed678872ab4501823a23b100c3c8328fb14f
morber <4497491+morber11@users.noreply.github.com>
2025-09-01 15:36:59 +0100

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: process.env.NODE_ENV === 'prod' ? {
+      '/api': 'http://localhost:8080',  
+    } : {},
+  },
 })
