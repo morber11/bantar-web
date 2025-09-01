826958e65e531348f4333b5ce334522684ecb15a
morber <4497491+morber11@users.noreply.github.com>
2025-09-01 15:38:56 +0100

    rename NODE_ENV to VITE_APP_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: process.env.NODE_ENV === 'prod' ? {
+    proxy: process.env.VITE_APP_ENV === 'prod' ? {
       '/api': 'http://localhost:8080',  
     } : {},
   },
