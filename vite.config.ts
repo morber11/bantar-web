d43292834725df60a15b2ba4ac8b370024784850
morber <4497491+morber11@users.noreply.github.com>
2025-09-01 15:49:12 +0100

    update proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Vite config
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: process.env.VITE_APP_ENV === 'prod' ? {
-      '/api': 'http://localhost:8080',  
-    } : {},
+    proxy: process.env.VITE_APP_ENV === 'prod'
+      ? { '/api': 'http://161.35.45.14:8080' }
+      : {},
   },
-})
+});
