a83fc916102ee3af811a5a3691446007826ae87e
morber <4497491+morber11@users.noreply.github.com>
2026-08-03 16:14:56 +0100

    fix fragile navigation

--- src/features/events/pages/EventsPage.tsx
+++ src/features/events/pages/EventsPage.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import RandomList from '../../../shared/ui/RandomList';
 import Sidebar from '../../navigation/components/Sidebar';
 import EventsSidebarContent from '../sidebarContent/EventsSidebarContent';
@@ -6,13 +6,11 @@ import { useFetchEvents } from '../hooks/useFetchEvents';
 import Spinner from '../../../shared/ui/Spinner';
 
 export default function EventsPage() {
-    const navigate = useNavigate();
     const { list, eventName, available, loading, error } = useFetchEvents({ simulateMountLoading: true });
 
     // redirect if events aren't available but the user tries to navigate manually
     if (!loading && (!available || error)) {
-        navigate('/', { replace: true });
-        return null;
+        return <Navigate to="/" replace />;
     }
 
     return (
